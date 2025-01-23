195882bd6e366e2689fb7d3873fdc616aaafc6c4
rahmaabdelkader2 <abdelkaderrahma7@gmail.com>
2025-01-23 09:34:57 +0200

    editing registers

--- MCAL/RCC/V1/RCC_config.h
+++ MCAL/RCC/V1/RCC_config.h
@@ -5,18 +5,19 @@
             RCC_HSE_RC
             RCC_HSI
             RCC_PLL    
-            
 */
 #define RCC_CLOCK_TYPE      RCC_HSE_CRYSTAL
 
-/* Options :RCC_PLL_IN_HSI_DIV_2
+/* Options : RCC_PLL_IN_HSI
             RCC_PLL_IN_HSE_DIV_2
             RCC_PLL_IN_HSE
 */
-/* Select value only if you have pll as input source clock*/
+/* Select value only if you have PLL as input source clock */
 #if RCC_CLOCK_TYPE == RCC_PLL
 #define RCC_PLL_INPUT    RCC_PLL_IN_HSE_DIV_2
-#define RCC_PLL_MUL_VAL   4
+#define RCC_PLL_M        8   /* PLL input division factor */
+#define RCC_PLL_N        336 /* PLL multiplication factor */
+#define RCC_PLL_P        2   /* PLL division factor for main system clock */
 #endif
 
 #endif
--- MCAL/RCC/V1/RCC_program.c
+++ MCAL/RCC/V1/RCC_program.c
@@ -1,49 +1,65 @@
 #include "stdtypes.h"
-#include"bit_math.h"
-#include"RCC_interface.h"
-#include"RCC_private.h"
-#include"RCC_config.h"
+#include "bit_math.h"
+#include "RCC_interface.h"
+#include "RCC_private.h"
+#include "RCC_config.h"
 
 void RCC_voidInitSysClock(void)
 {
     #if RCC_CLOCK_TYPE == RCC_HSE_CRYSTAL
-        RCC_CR =   0x00010000; /* Enable HSE with no bypass */
-        RCC_CFGR = 0x00000001;
+        RCC_CR |= 0x00010000; /* Enable HSE with no bypass */
+        while (!(RCC_CR & 0x00020000)); /* Wait for HSE to be ready */
+        RCC_CFGR |= 0x00000001; /* Set HSE as system clock */
     #elif RCC_CLOCK_TYPE == RCC_HSE_RC
-        RCC_CR   = 0x00050000; /* Enable HSE with bypass */
-        RCC_CFGR = 0x00000001;
+        RCC_CR |= 0x00050000; /* Enable HSE with bypass */
+        while (!(RCC_CR & 0x00020000)); /* Wait for HSE to be ready */
+        RCC_CFGR |= 0x00000001; /* Set HSE as system clock */
     #elif RCC_CLOCK_TYPE == RCC_HSI
-        RCC_CR   = 0x00000081; /* Enable HSI + Trimming = 0 */
-        RCC_CFGR = 0x00000000;
+        RCC_CR |= 0x00000001; /* Enable HSI */
+        while (!(RCC_CR & 0x00000002)); /* Wait for HSI to be ready */
+        RCC_CFGR |= 0x00000000; /* Set HSI as system clock */
     #elif RCC_CLOCK_TYPE == RCC_PLL
-        #if RCC_PLL_INPUT == RCC_PLL_IN_HSI_DIV_2
-        RCC_CR   = 0x01000000; /* Enable HSI/2 */
-        RCC_CFGR = 0x00000002;
-        #elif RCC_PLL_INPUT == RCC_PLL_IN_HSE_DIV_2
-        RCC_CR   = 0x01010000; /* Enable HSE/2 */
-        RCC_CFGR = 0x00030002;
+        RCC_CR &= ~0x01000000; /* Disable PLL before configuration */
+        
+        #if RCC_PLL_INPUT == RCC_PLL_IN_HSI
+            RCC_PLLCFGR = (RCC_PLLCFGR & ~(0x3 << 22)) | (0x0 << 22); /* HSI as PLL source */
         #elif RCC_PLL_INPUT == RCC_PLL_IN_HSE
-        RCC_CR   = 0x01000000; /* Enable HSE */
-        RCC_CFGR = 0x00010002;
-        #endif 
+            RCC_PLLCFGR = (RCC_PLLCFGR & ~(0x3 << 22)) | (0x1 << 22); /* HSE as PLL source */
+        #elif RCC_PLL_INPUT == RCC_PLL_IN_HSE_DIV_2
+            RCC_PLLCFGR = (RCC_PLLCFGR & ~(0x3 << 22)) | (0x1 << 22); /* HSE as PLL source */
+            RCC_PLLCFGR |= (1 << 4); /* Divide HSE by 2 */
+        #endif
+
+        RCC_PLLCFGR |= (RCC_PLL_M << 0) | (RCC_PLL_N << 6) | (RCC_PLL_P << 16);
+        RCC_CR |= 0x01000000; /* Enable PLL */
+        while (!(RCC_CR & 0x02000000)); /* Wait for PLL to be ready */
+        RCC_CFGR |= 0x00000002; /* Set PLL as system clock */
     #else
-        #error("You chosed wrong clock type")
+        #error("You chose a wrong clock type")
     #endif
 }
 
-void RCC_voidEnableClock(u8 copy_u8BusId   ,u8 copy_u8PerId)
+void RCC_voidEnableClock(u8 copy_u8BusId, u8 copy_u8PerId)
 {
-    if(copy_u8PerId<=31){
-        switch(copy_u8BusId){
-        case RCC_AHB  : SET_BIT(RCC_AHBENR,copy_u8PerId); break;
-        case RCC_APB1 : SET_BIT(RCC_APB1ENR,copy_u8PerId); break;
-        case RCC_APB2 : SET_BIT(RCC_APB2ENR,copy_u8PerId); break;
-        default: /*return error*/ break;
+    if (copy_u8PerId <= 31) {
+        switch (copy_u8BusId) {
+        case RCC_AHB1:
+            SET_BIT(RCC_AHB1ENR, copy_u8PerId);
+            break;
+        case RCC_AHB2:
+            SET_BIT(RCC_AHB2ENR, copy_u8PerId);
+            break;
+        case RCC_APB1:
+            SET_BIT(RCC_APB1ENR, copy_u8PerId);
+            break;
+        case RCC_APB2:
+            SET_BIT(RCC_APB2ENR, copy_u8PerId);
+            break;
+        default:
+            /* Return error */
+            break;
+        }
+    } else {
+        /* Return error */
     }
-    }
-    else{
-        //return error
-    }
-
-    
-}  
+}
